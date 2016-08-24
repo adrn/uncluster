cf420613a88ad3279078e0a07de869c7543fa5a9
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-24 09:29:30 -0400

    get output path

--- uncluster/__init__.py
+++ uncluster/__init__.py
@@ -1,1 +1,11 @@
 from .gnedin_mass_radius import *
+
+def get_output_path(f):
+    import os
+    from os.path import abspath, split, join, exists
+
+    _root_path = abspath(join(split(abspath(f))[0], ".."))
+    OUTPUT_PATH = join(_root_path, "output")
+    if not exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
+    return OUTPUT_PATH
