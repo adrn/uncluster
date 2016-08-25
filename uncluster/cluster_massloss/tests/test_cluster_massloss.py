caffa541dc9bf2011c723d9ff1d712810e360d4f
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-25 11:36:37 -0400

    add empty tests

---
+++ uncluster/cluster_massloss/tests/test_cluster_massloss.py
@@ -0,0 +1,12 @@
+from __future__ import division, print_function
+
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+import os
+import sys
+
+# Third-party
+from astropy import log as logger
+import astropy.units as u
+import numpy as np
