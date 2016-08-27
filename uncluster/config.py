da0ee05f41ffbcdb74e9daf7031ef46095ff6997
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-27 11:54:45 -0400

    remove max r

--- uncluster/config.py
+++ uncluster/config.py
@@ -9,7 +9,6 @@ f_gc = 0.012 # fraction of total stellar mass in GCs
 M_min = 1E4 * u.Msun
 M_max = 1E7 * u.Msun
 M_tot = 5E10 * u.Msun
-r_max = 250. * u.kpc # changed from G14 value
 t_evolve = 11.5 * u.Gyr
 
 # Background Milky Way potential
