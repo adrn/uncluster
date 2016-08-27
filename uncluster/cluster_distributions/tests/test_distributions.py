eb966626e0cd2ec4f394fc70ea7bdced3fb6700e
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-27 11:57:20 -0400

    few more trivial unit tests

--- uncluster/cluster_distributions/tests/test_distributions.py
+++ uncluster/cluster_distributions/tests/test_distributions.py
@@ -12,9 +12,25 @@ def test_apw():
 
     r = sample_radii(size=128)
     assert np.isfinite(r).all()
+    assert r.unit == u.kpc
+
+    m = sample_masses(size=128)
+    assert np.isfinite(m).all()
+    assert m.unit == u.Msun
+
+    prob = [gc_prob_density(rr) for rr in r.value]
+    assert np.isfinite(prob).all()
 
 def test_gnedin():
     from ..gnedin import sample_radii, sample_masses, gc_prob_density
 
     r = sample_radii(size=128)
     assert np.isfinite(r).all()
+    assert r.unit == u.kpc
+
+    m = sample_masses(size=128)
+    assert np.isfinite(m).all()
+    assert m.unit == u.Msun
+
+    prob = [gc_prob_density(rr) for rr in r.value]
+    assert np.isfinite(prob).all()
