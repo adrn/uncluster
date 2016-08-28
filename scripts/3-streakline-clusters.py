78478dfd29afbac0b2d43d396c0fb11fb2b13cac
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-08-28 19:59:48 -0400

    move and rename

--- scripts/3-streakline-clusters.py
+++ scripts/3-streakline-clusters.py
@@ -1,18 +1,6 @@
 """
-    Given masses, mass-loss rates, orbital radii, eccentricities, and destruction times for an
-    initial population of clusters computed from reproducing the simulation in Gnedin et al.
-    (2014), generate Streakline+scatter+full-disruption simulations for each globular cluster
-    stream.
-
-    This code makes the following simplifying assumptions:
-        - Dynamical friction is neglected: we are interested in clusters with r > 5 kpc, where,
-          even for the most massive globular clusters, dynamical friction has a negligible effect.
-        -
-
-    TODO:
-    - In next script, take output from this file and paint stellar population, abundances on to
-      star particles in each "stream"
-
+    Solve for the mass-loss rates and destruction times for each cluster given
+    its eccentricity, initial mass, and initial radius.
 """
 
 from __future__ import division, print_function
@@ -24,39 +12,96 @@ from os.path import join, exists
 
 # Third-party
 from astropy import log as logger
+from astropy.table import QTable
 import astropy.units as u
 import h5py
+import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import odeint
 
-import gala.coordinates as gc
-import gala.dynamics as gd
-import gala.integrate as gi
-import gala.potential as gp
-from gala.units import galactic
-
-from uncluster import get_output_path
+from uncluster import get_output_path, get_plot_path
 from uncluster.conf import t_evolve
-from uncluster.potential import potential as pot
+from uncluster.cluster_massloss import solve_mass_radius
 OUTPUT_PATH = get_output_path(__file__)
+PLOT_PATH = get_plot_path(__file__)
 
-def main(gc_evo_filename, output_filename):
-    if not exists(gc_evo_filename):
-        raise IOError("File '{}' does not exist -- have you run 2-evolve-clusters.py?"
-                      .format(gc_evo_filename))
+def main(gc_props_filename, output_filename):
+    if not exists(gc_props_filename):
+        raise IOError("File '{}' does not exist -- have you run 1-make-cluster-props.py?"
+                      .format(gc_props_filename))
+    gc_props = QTable.read(gc_props_filename, format='ascii.ecsv')
 
-    with h5py.File(gc_evo_filename) as f:
-        for i in range(f.attrs['n']):
-            g = f['clusters/{}'.format(i)]
+    t_grid = np.linspace(0., t_evolve.to(u.Gyr).value, 4096)
+    gc_mass = gc_props['mass'].to(u.Msun).value
+    gc_radius = gc_props['radius'].to(u.kpc).value
 
-            t = f['time'][:g.attrs['disrupt_idx']+1]
-            m = g['mass'][:]
-            r = np.mean(g['radius'][:])
-            ecc = g.attrs['ecc']
-            break
+    disrupt_idx, m, r = solve_mass_radius(gc_mass, gc_radius, t_grid)
 
+    t_disrupt = t_grid[disrupt_idx+1]
 
-    pot
+    # set disruption time to NaN for those that don't disrupt
+    t_disrupt[t_disrupt == t_evolve.to(u.Gyr).value] = np.nan
+
+    logger.info("{}/{} clusters survived".format(np.isnan(t_disrupt).sum(), len(t_disrupt)))
+
+    # Now make some plots:
+
+    # - mass and density distribution of surviving clusters
+    final_m = np.array([m[i][disrupt_idx[i]] for i in range(len(gc_props))])
+    final_r = np.array([r[i][disrupt_idx[i]] for i in range(len(gc_props))])
+
+    fig,axes = plt.subplots(1,2,figsize=(12,6))
+
+    axes[0].hist(gc_mass, bins=np.logspace(4,7.1,9), alpha=0.3)
+    axes[0].hist(final_m[np.isnan(t_disrupt)], bins=np.logspace(3,7.1,12), alpha=0.3)
+
+    axes[0].set_xscale('log')
+    axes[0].set_yscale('log')
+    axes[0].set_xlim(1E3, 3E7)
+    axes[0].set_ylim(5E-1, 1E4)
+    axes[0].set_xlabel(r"Mass [${\rm M}_\odot$]")
+    axes[0].set_ylabel(r"$N$")
+
+    bins = np.logspace(-1.,2.,16)
+    H,_ = np.histogram(gc_radius, bins=bins)
+    # data_H,_ = np.histogram(harris_gc_dist, bins=bins)
+
+    V = 4/3*np.pi*(bins[1:]**3 - bins[:-1]**3)
+    bin_cen = (bins[1:]+bins[:-1])/2.
+    axes[1].plot(bin_cen, H/V, ls='--', marker=None)
+    # axes[1].errorbar(bin_cen, data_H/V, np.sqrt(data_H)/V,
+    #                  color='k', marker='o', ecolor='#666666', linestyle='none')
+
+    H_f,_ = np.histogram(final_r[np.isnan(t_disrupt)], bins=bins)
+    axes[1].plot(bin_cen, H_f/V, ls='--', marker=None)
+
+    axes[1].set_xscale('log')
+    axes[1].set_yscale('log')
+    axes[1].set_xlim(1E-1, 1E2)
+    axes[1].set_ylim(1E-7, 1E2)
+    axes[1].set_xlabel(r"$r$ [kpc]")
+    axes[1].set_ylabel('GC density [kpc$^{-3}$]')
+
+    fig.tight_layout()
+    fig.savefig(join(PLOT_PATH, "initial-final-mass-density.pdf"))
+
+    # -
+
+    # write to hdf5 file
+    with h5py.File(output_filename, 'w') as f:
+        f.create_dataset('time', data=t_grid)
+        f.attrs['n'] = len(t_disrupt)
+
+        cl = f.create_group('clusters')
+        for i in range(len(gc_props)):
+            # set mass to 0 at disruption index
+            if not np.isnan(t_disrupt[i]):
+                m[i][disrupt_idx[i]] = 0.
+
+            g = cl.create_group(str(i))
+            g.create_dataset('mass', data=m[i][:disrupt_idx[i]+1])
+            g.create_dataset('radius', data=r[i][:disrupt_idx[i]+1])
+            g.attrs['t_disrupt'] = t_disrupt[i]
+            g.attrs['disrupt_idx'] = disrupt_idx[i]
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -72,11 +117,11 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--seed", dest="seed", default=8675309,
                         type=int, help="Random number generator seed.")
 
-    parser.add_argument("--gc-evolution", dest="gc_evo_filename", default=None,
-                        type=int, help="Path to HDF5 file containing the mass and radius "
-                                       "evolution for each cluster.")
-    # parser.add_argument("--output", dest="output_filename", default=None,
-    #                     type=int, help="Output path for the HDF5 file written by this script.")
+    parser.add_argument("--gc-properties", dest="gc_props_filename", default=None,
+                        type=int, help="Path to ECSV file containing the sampled "
+                                       "cluster properties (i.e. mass and orbital radius)")
+    parser.add_argument("--output", dest="output_filename", default=None,
+                        type=int, help="Output path for the HDF5 file written by this script.")
 
     args = parser.parse_args()
 
@@ -88,13 +133,13 @@ if __name__ == '__main__':
     else:
         logger.setLevel(logging.INFO)
 
-    gc_evo_filename = args.gc_evo_filename
-    if gc_evo_filename is None:
-        gc_evo_filename = join(OUTPUT_PATH, "2-gc-evolution.hdf5")
+    gc_props_filename = args.gc_props_filename
+    if gc_props_filename is None:
+        gc_props_filename = join(OUTPUT_PATH, "1-gc-properties.ecsv")
 
-    # output_filename = args.output_filename
-    # if output_filename is None:
-    #     output_filename = join(OUTPUT_PATH, "2-gc-evolution.hdf5")
+    output_filename = args.output_filename
+    if output_filename is None:
+        output_filename = join(OUTPUT_PATH, "2-gc-evolution.hdf5")
 
     np.random.seed(args.seed)
-    main(gc_evo_filename, None)
+    main(gc_props_filename, output_filename)
