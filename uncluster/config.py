5f1d589c42df0f336e56995b9eaa302fbf778b9f
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-17 16:48:59 -0500

    change parameters to use best-fit values

--- uncluster/config.py
+++ uncluster/config.py
@@ -1,23 +1,18 @@
-from math import log, sqrt
+import astropy.cosmology as ac
 import astropy.units as u
-from astropy.constants import G
-import gala.potential as gp
-from gala.units import galactic
+
+# use Planck 2015 cosmology
+cosmology = ac.Planck15
 
 # from Gnedin et al. 2014
 f_gc = 0.012 # fraction of total stellar mass in GCs
 M_min = 1E4 * u.Msun
 M_max = 1E7 * u.Msun
 M_tot = 5E10 * u.Msun
-t_evolve = 11.5 * u.Gyr
 
-# Background Milky Way potential
-mw_potential = gp.CCompositePotential()
-mw_potential['disk'] = gp.MiyamotoNagaiPotential(m=4E10, a=3.5, b=0.14, units=galactic)
-mw_potential['bulge'] = gp.HernquistPotential(m=1E10, c=1.1, units=galactic)
+# amount of time to evolve clusters
+z_max = 3.
+t_max = cosmology.lookback_time(z_max)
 
-# for DM halo potential
-M_h = 1E12 * u.Msun
-rs_h = 20. * u.kpc
-v_c = sqrt(((log(2.) - 0.5) * (G * M_h / rs_h)).decompose(galactic).value)
-mw_potential['halo'] = gp.SphericalNFWPotential(v_c=v_c, r_s=rs_h, units=galactic)
+# Assumed halo concentration at redshift 0
+halo_c0 = 15.
--- uncluster/potential/core.py
+++ uncluster/potential/core.py
@@ -9,9 +9,8 @@ from .mwpotential import (GrowingHernquistPotential,
                           GrowingSphericalNFWPotential)
 
 mw_potential = gp.CCompositePotential()
-mw_potential['nucl'] = GrowingHernquistPotential(m0=2E9, c0=0.1, units=galactic)
+mw_potential['nucl'] = GrowingHernquistPotential(m0=1.66E9, c0=65*u.pc, units=galactic) # best-fit values
 mw_potential['bulge'] = GrowingHernquistPotential(m0=5E9, c0=1., units=galactic)
 mw_potential['disk'] = GrowingMiyamotoNagaiPotential(m0=6.8E10*u.Msun, a0=3*u.kpc, b=280*u.pc,
                                                      units=galactic)
-mw_potential['halo'] = GrowingSphericalNFWPotential(m0=6E11, r_s=16., units=galactic)
-
+mw_potential['halo'] = GrowingSphericalNFWPotential(m0=6.07E11, r_s=16.61, units=galactic) # best-fit values
