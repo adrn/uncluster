20af2b63af2d9400e43ee44ee3a19eb457f96b6b
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-18 09:18:08 -0500

    some new hacks to generate code

--- scripts/setup-potential.py
+++ scripts/setup-potential.py
@@ -1,15 +1,19 @@
 """
-Fit the potential model to Milky Way enclosed mass measurements compiled by
-Oleg Gnedin and Andreas Kuepper. The only free parameters are the halo mass,
-halo scale radius, nucleus mass, and nucleus scale radius. The best-fit values
-from this script are then dumped into uncluster/potential/core.py to be used
-for the rest of the project.
+1. Fit the potential model to Milky Way enclosed mass measurements compiled by Oleg Gnedin and
+   Andreas Kuepper. The only free parameters are the halo mass, halo scale radius, nucleus mass,
+   and nucleus scale radius. The best-fit values from this script are then dumped into
+   uncluster/potential/core.py to be used for the rest of the project.
+
+2. Generate arrays that specify the time-evolution of the potential parameters for fast
+   interpolation later.
+
 """
 
 # Standard library
 from os.path import exists, abspath, join
 
 # Third-party
+import astropy.cosmology as ac
 from astropy.constants import G
 import astropy.units as u
 import matplotlib.pyplot as plt
@@ -17,6 +21,12 @@ import numpy as np
 from scipy.optimize import leastsq
 import gala.potential as gp
 from gala.units import galactic
+from scipy.interpolate import interp1d
+
+# These are the only configurable items - if you change them here, you must also change them in
+#   uncluster/config.py
+cosmology = ac.Planck15
+halo_concentration = 15. # MAGIC NUMBER - assumption
 
 # Project
 def get_potential(log_M_h, log_r_s, log_M_n, log_a):
@@ -35,6 +45,15 @@ def main(plot=False):
     if not exists(data_path):
         raise IOError("Could not find data path -- you should run this from the "
                       "'scripts' directory.")
+
+    potential_path = abspath("../uncluster/potential/src/")
+    if not exists(potential_path):
+        raise IOError("Could not find path to potential subpackage -- you should "
+                      "run this script from the 'scripts' directory.")
+
+    ##########################################################################
+    # 1. Get the best-fit potential parameters
+    #
 
     # load tables of mass enclosed / circular velocity measurements
     gnedin_tbl = np.genfromtxt(join(data_path, "gnedin_mwmass_tbl.txt"),
@@ -121,6 +140,89 @@ def main(plot=False):
     print("r_s = {:.2f} * u.kpc".format(r_s))
     print("m_n = {:.2e} * u.Msun".format(m_n))
     print("c_n = {:.2f} * u.pc".format(a))
+
+    ##########################################################################
+    # 2. Now solve for the cosmological evolution of the parameters:
+    #
+    def F(c):
+        return np.log(1 + c) - c/(1+c)
+
+    # Global configuration
+    M_vir0 = F(halo_concentration) * fit_potential['halo'].parameters['m']
+    n_grid = 256
+    grid_z_max = 4. # generate the grid up to redshift 4 even though we will only evolve from z=3
+
+    # Use astropy.cosmology to compute the evolution of the virial mass and
+    #   virial radius of our halo potential. This script prints the grids so
+    #   the C code (uncluster/potential/src/components.c) can interpolate to get
+    #   the quantities at any input time.
+    t_grid = np.linspace(0, 1., n_grid) * cosmology.lookback_time(grid_z_max)
+    z_grid = np.array([0.] + [ac.z_at_value(cosmology.lookback_time, tt) for tt in t_grid[1:]])
+    t_grid = t_grid[::-1]
+    z_grid = z_grid[::-1]
+
+    def Delta(z):
+        """ An approximation thanks to Dekel & Birnboim 2006 (see appendix) """
+        return (18*np.pi**2 - 82*cosmology.Ode(z) - 39*cosmology.Ode(z)**2) / cosmology.Om(z)
+
+    def M_vir(z):
+        return M_vir0 * np.exp(-0.8*z)
+
+    def R_vir(z):
+        _Delta = (Delta(z) / 200)**(-1/3.)
+        _Om = (cosmology.Om(z) / 0.3)**(-1/3.)
+        _h2 = (cosmology.h / 0.7)**(-2/3.)
+        return 309 * (M_vir(z)/(1E12*u.Msun))**(1/3) * _Delta * _Om * _h2 / (1 + z) * u.kpc
+
+    print("")
+    _M_vir0 = M_vir(0).to(u.Msun).value
+    _R_vir0 = R_vir(0).to(u.kpc).value
+    print("Mvir at z=0: {:.2e} Msun".format(_M_vir0))
+    print("Rvir at z=0: {:.2f} kpc".format(_R_vir0))
+
+    # Oof, what has my life come to -- generating C code
+    lines = []
+    var_names = []
+
+    lines.append(str((-t_grid.to(u.Myr).value).tolist()))
+    var_names.append('t_grid[]')
+
+    lines.append(str(z_grid.tolist()))
+    var_names.append('z_grid[]')
+
+    lines.append(str(R_vir(z_grid).to(u.kpc).value.tolist()))
+    var_names.append('R_vir_grid[]')
+
+    lines.append(str(M_vir(z_grid).to(u.Msun).value.tolist()))
+    var_names.append('M_vir_grid[]')
+
+    # Now we need to make a grid for the stellar mass evolution. We use the
+    #   star formation rates from Behroozi et al. 2013 for a 10^12 solar mass
+    #   virial mass halo.
+    path = join(data_path, "sm_hist_rel_12.0.dat")
+    d = np.genfromtxt(path, names=['a', 'f_star'], usecols=[0,1])
+
+    ifunc = interp1d(1/d['a'] - 1, d['f_star'], kind='linear')
+    f_star = ifunc(z_grid)
+    lines.append(str(f_star.tolist()))
+    var_names.append('fstar_grid[]')
+
+    for i,(line,name) in enumerate(zip(lines, var_names)):
+        line = line.replace("[", "{")
+        line = line.replace("]", "}")
+        lines[i] = "static double {} = {};\n".format(name, line)
+
+    top_lines = [
+        '// vvv --- THIS IS AUTO-GENERATED CODE - see scripts/setup-potential.py --- vvv\n',
+        'static double const M_vir0 = {:.5e};\n'.format(_M_vir0),
+        'static double const R_vir0 = {:.5f};\n'.format(_R_vir0),
+        'static int const interp_grid_size = {};\n'.format(n_grid)
+    ]
+
+    lines = top_lines + lines
+    lines = lines + ['// ^^^ --- THIS IS AUTO-GENERATED CODE - see scripts/setup-potential.py --- ^^^\n']
+    with open(join(potential_path, "cosmo_arrays.h"), "w") as f:
+        f.writelines(lines)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
