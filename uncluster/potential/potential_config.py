b51adafd7040b93cceaec22f60a389d6dd23fdf9
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-19 12:50:49 -0500

    add auto-generated files to repo

---
+++ uncluster/potential/potential_config.py
@@ -0,0 +1,7 @@
+# vvv --- THIS IS AUTO-GENERATED CODE - see scripts/setup-potential.py --- vvv
+import astropy.units as u
+m_h = 5.41e+11 * u.Msun
+r_s = 15.65 * u.kpc
+m_n = 1.71e+09 * u.Msun
+c_n = 66.57 * u.pc
+# ^^^ --- THIS IS AUTO-GENERATED CODE - see scripts/setup-potential.py --- ^^^
