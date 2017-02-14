b5f11f6be11541c1331bd068a3ab9c0fbb8f35b8
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-02-14 15:05:32 -0800

    specify mvir0 not concentration

--- uncluster/config.py
+++ uncluster/config.py
@@ -5,15 +5,13 @@ import astropy.units as u
 # If you change these, you must also re-run `scripts/setup_potential.py`
 #
 cosmology = ac.Planck15
-halo_concentration = 15. # MAGIC NUMBER - assumption
+Mvir0 = 1E12 * u.Msun # MAGIC NUMBER - assumption
 #
 ##############################################################################
 
 # from Gnedin et al. 2014
-f_gc = 0.012 # fraction of total stellar mass in GCs
 M_min = 1E4 * u.Msun
 M_max = 1E7 * u.Msun
-M_tot = 5E10 * u.Msun
 
 # amount of time to evolve clusters
 z_max = 3.
--- uncluster/potential/src/cosmo_helper.h
+++ uncluster/potential/src/cosmo_helper.h
@@ -1,3 +1,3 @@
 // vvv --- THIS IS AUTO-GENERATED CODE - see scripts/setup-potential.py --- vvv
-static double const M_vir0 = 1.00189401e+12;
+static double const M_vir0 = 1.0e+12;
 // ^^^ --- THIS IS AUTO-GENERATED CODE - see scripts/setup-potential.py --- ^^^
